ee9fc6cdcb280767e467d1a630d8e6ba79680f23
Cristian Morales <cristianmorales1198@gmail.com>
2023-05-13 00:15:07 -0500

    Adding reset and backspace function

--- script.js
+++ script.js
@@ -35,57 +35,77 @@ let secondNumber = '';
 let displayValue = '';
 
 function appendNumber(number) {
+    calcScreen.textContent += number;
     if (!operator) {
         firstNumber += number.toString();
     } else {
         secondNumber += number.toString();
     }
-    updateDisplay();
-}
+  }
 
-function setOperator(selectedOperator) {
-    operator = selectedOperator;
-    updateDisplay();
-}
-
-function calculate() {
-    if (firstNumber && operator && secondNumber) {
-        const result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
-        displayValue = result.toString();
-        firstNumber = '';
-        operator = '';
-        secondNumber = '';
-        updateDisplay()
-    }
-}
-
-function updateDisplay() {
-    const display = document.querySelector('.calc--screen');
+  function setOperator(selectedOperator) {
     if (operator) {
-        display.textContent = `${firstNumber} ${operator} ${secondNumber}`;
-    } else {
-        display.textContent = displayValue;
+      calculate();
     }
-}
+    operator = selectedOperator;
+    displayValue = '';
+    updateDisplay();
+  }  
 
-const numberButtons = document.querySelectorAll('[data-btn]');
-numberButtons.forEach((button) => {
+  function calculate() {
+    if (firstNumber && operator && secondNumber) {
+      const result = operate(operator, parseFloat(firstNumber), parseFloat(secondNumber));
+      displayValue = result.toString();
+      firstNumber = result.toString();
+      operator = '';
+      secondNumber = '';
+      updateDisplay();
+    }
+  }  
+  
+  function updateDisplay() {
+    if (operator && secondNumber) {
+      calcScreen.textContent = `${firstNumber} ${operator} ${secondNumber}`;
+    } else if (operator) {
+      calcScreen.textContent = `${firstNumber} ${operator}`;
+    } else {
+      calcScreen.textContent = displayValue;
+    }
+  }
+  
+  const numberButtons = document.querySelectorAll('[data-btn]');
+  numberButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        const number = button.getAttribute('data-btn');
-        appendNumber(number)
+      const number = button.getAttribute('data-btn');
+      appendNumber(number);
     });
-    
-});
+  });
+  
 
-const operatorButtons = document.querySelectorAll('.operator');
-operatorButtons.forEach((button) => {
+  const operatorButtons = document.querySelectorAll('.operator');
+  operatorButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        const selectedOperator = button.getAttribute('data-btn');
-        setOperator(selectedOperator);
+      const selectedOperator = button.getAttribute('data-btn');
+      setOperator(selectedOperator);
     });
-});
+  });
+  
 
 const equalsButton = document.querySelector('[data-equals]');
 equalsButton.addEventListener('click', calculate);
+
+
+const resetBtn = document.querySelector('[data-reset]');
+const calcScreen = document.querySelector('.calc--screen');
+
+resetBtn.addEventListener('click', () => {
+  calcScreen.textContent = '';
+});
+
+const deleteButton = document.querySelector('[data-delete]');
+
+deleteButton.addEventListener('click', () => {
+  calcScreen.textContent = calcScreen.textContent.slice(0, -1);
+});
 
 updateDisplay()
